e618c69d1bbb28d03a4dc94cceaa2f7d5791d552
johann Bacha <johannbacha@gmail.com>
2022-12-26 11:39:52 +0100

    probleme connection api

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "client",
     "contract",
     "config",
@@ -44,6 +45,11 @@ INSTALLED_APPS = [
     "event",
     "user",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
